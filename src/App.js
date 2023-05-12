ea03b6e470e9023f7ef7adb46fb2a575f3b6fc55
Ramsha Iqbal <90575580+Ramsha42001@users.noreply.github.com>
2023-05-12 23:24:02 +0530

    added jokes file

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Jokes from "./components/Jokes"
+
+ function App()
+ {
+  return (
+    <Jokes />
+  )
+ }
+
+ export default App;
---
+++ src/components/Jokes.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Jokes({para})
+{
+      <p>{para}</p>
+}
+
+
+const joke=
+{
+    para: "What do Alexander the Great and Winnie the Pooh have in common? Same middle name."
+}
+
+export default Jokes;
